510d707f815dc6cbf93d7d69941ccfcefae14b0c
Daniel Filho <filhodan@gmail.com>
2024-04-30 00:24:37 -0300

    Adding secondary and tertiary buttons to the component library

--- Nectar Online Groceries/Components/NOGButton.swift
+++ Nectar Online Groceries/Components/NOGButton.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class NOGButton: UIButton {
-
-    var style: String!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -17,10 +15,13 @@ class NOGButton: UIButton {
     
     init(title: String, style: String) {
         super.init(frame: .zero)
-        self.style = style
         
         if style == "Primary" {
             configurePrimaryButton(title: title)
+        } else if style == "Secondary" {
+            configureSecondaryButton(title: title)
+        } else {
+            configureTertiaryButton(title: title)
         }
     }
     
@@ -36,6 +37,29 @@ class NOGButton: UIButton {
         
         self.setTitle(title, for: .normal)
         titleLabel?.textColor = UIColor.white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+    
+    private func configureSecondaryButton(title: String) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius      = 19
+        layer.backgroundColor   = UIColor.systemGray6.cgColor
+        tintColor               = UIColor.systemGreen
+        
+        setTitleColor(.systemGreen, for: .normal)
+        //setImage(icon, for: .normal)
+        setTitle(title, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+    
+    private func configureTertiaryButton(title: String) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        setTitle(title, for: .normal)
+        setTitleColor(UIColor.black, for: .normal)
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
     }
 }
--- Nectar Online Groceries/View/AccountVC.swift
+++ Nectar Online Groceries/View/AccountVC.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class AccountVC: UIViewController {
 
+    let logoutUIButton = NOGButton(title: "Log out", style: "Secondary")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        configureLogoutUIButton()
+    }
+    
+    func configureLogoutUIButton() {
+        view.addSubview(logoutUIButton)
+        
+        NSLayoutConstraint.activate([
+            logoutUIButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            logoutUIButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            logoutUIButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            logoutUIButton.heightAnchor.constraint(equalToConstant: 67)
+        ])
+        
     }
 }
